cb56bb91279736c39732a683fdc23de754b9bb18
Hasherin <37940226+Hasherin@users.noreply.github.com>
2025-04-24 21:49:35 +0200

    V3

---
+++ script.js
@@ -0,0 +1,9 @@
+function copyToClipboard(text) {
+    navigator.clipboard.writeText(text)
+      .then(() => {
+        alert('IP cím kimásolva');
+      })
+      .catch(err => {
+        alert('Nem sikerült kimásolni az IP címet: ', err);
+      });
+  }
