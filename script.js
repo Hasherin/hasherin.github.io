3aa0a07f8079e483b46ee1c899b08502d73ad6b3
Nani0002 <molnardani002@gmail.com>
2025-04-25 23:14:03 +0200

    V5

--- script.js
+++ script.js
@@ -1,9 +1,46 @@
+let mode = "light";
+let timeout;
+const images = ["comp", "parl", "shrine", "spawn"];
+let currentImage = 0
+
+window.addEventListener("load", init, false);
+
 function copyToClipboard(text) {
-  navigator.clipboard.writeText(text)
-    .then(() => {
-      alert('IP cím kimásolva');
-    })
-    .catch(err => {
-      alert('Nem sikerült kimásolni az IP címet: ', err);
-    });
+	navigator.clipboard
+		.writeText(text)
+		.then(() => {
+			alert("IP cím kimásolva");
+		})
+		.catch((err) => {
+			alert("Nem sikerült kimásolni az IP címet: ", err);
+		});
+}
+
+function init() {
+	mode = localStorage.getItem("mode");
+    document.querySelector("#theme-switch").addEventListener('click', () => switchBg(), false);
+	if (mode == null) {
+		mode = window.matchMedia("(prefers-color-scheme: dark)").matches
+			? "dark"
+			: "light";
+		localStorage.setItem("mode", mode);
+	}
+    currentImage = 0;
+	document.body.style.backgroundImage = `url('images/${mode}/${images[currentImage]}-${mode}.png')`;
+	setBg();
+}
+
+function setBg() {
+    document.body.style.backgroundImage = `url('images/${mode}/${images[currentImage]}-${mode}.png')`;
+	timeout = setInterval(() => {
+		currentImage = (currentImage + 1) % 4;
+		document.body.style.backgroundImage = `url('images/${mode}/${images[currentImage]}-${mode}.png')`;
+	}, 10000);
+}
+
+function switchBg() {
+    mode = mode === "light" ? "dark" : "light";    
+	localStorage.setItem("mode", mode);
+	clearInterval(timeout);
+	setBg();
 }
