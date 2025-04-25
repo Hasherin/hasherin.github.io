70dbe6fc89057dbeb86af18432078c1bcc83ef7e
Hasherin <37940226+Hasherin@users.noreply.github.com>
2025-04-25 22:25:13 +0200

    V4

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function copyToClipboard(text) {
-    navigator.clipboard.writeText(text)
-      .then(() => {
-        alert('IP cím kimásolva');
-      })
-      .catch(err => {
-        alert('Nem sikerült kimásolni az IP címet: ', err);
-      });
-  }
+  navigator.clipboard.writeText(text)
+    .then(() => {
+      alert('IP cím kimásolva');
+    })
+    .catch(err => {
+      alert('Nem sikerült kimásolni az IP címet: ', err);
+    });
+}
